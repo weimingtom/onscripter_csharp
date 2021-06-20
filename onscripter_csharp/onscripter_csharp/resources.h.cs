ac578b413ee7bb81e0337199e1e0dfe719a2c44a
weimingtom <weimingtom@qq.com>
2021-06-20 09:26:41 +0800

    all header files

---
+++ onscripter_csharp/onscripter_csharp/resources.h.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: wmt
+ * Date: 2021-6-20
+ * Time: 9:19
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace onscripter_csharp
+{
+	public partial class ONScripter
+	{
+		// A system for storing files within the executable rather than the
+		// data directory.
+		
+		//#ifndef RESOURCES_H
+		//#define RESOURCES_H
+		
+//		struct InternalResource {
+//		    const char* filename;
+//		    const unsigned char* buffer;
+//		    long int size;
+//		};
+//		
+//		extern const InternalResource* getResource(const char* filename);
+		
+		//#endif
+	}
+}
---
+++ onscripter_csharp/onscripter_csharp/winres.h.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: wmt
+ * Date: 2021-6-20
+ * Time: 9:24
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace onscripter_csharp
+{
+	public partial class ONScripter
+	{
+		//#ifndef __WINRES_H__
+		//#define __WINRES_H__
+		
+		private const int ONSCRICON = 101;
+		
+		//#endif
+	}
+}
