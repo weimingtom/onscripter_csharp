094d3a303f9d77955895c17997224831569c140a
weimingtom <weimingtom@qq.com>
2021-06-20 08:38:41 +0800

    mod

---
+++ onscripter_csharp/onscripter_csharp/MainForm.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: wmt
+ * Date: 2021-6-20
+ * Time: 8:29
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace onscripter_csharp
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
---
+++ onscripter_csharp/onscripter_csharp/version.h.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: wmt
+ * Date: 2021-6-20
+ * Time: 8:34
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace onscripter_csharp
+{
+	public partial class ONScripter
+	{
+		public static string xstr(string s) { return str(s); }
+		public static string str(string s) { return s; }
+		
+		public const string VER_NUMBER = "20110314-en";
+		public static string ONS_VERSION = xstr(VER_NUMBER);
+		public const int NSC_VERSION = 294;
+	}
+}
