5d143f100bc7682b8f513a7ccc0337d47099d024
weimingtom <weimingtom@qq.com>
2021-08-15 04:42:12 +0800

    onscripter.cpp

---
+++ onscripter_csharp/onscripter_csharp/ONScripterLabel.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: wmt
+ * Date: 2021-8-15
+ * Time: 4:39
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace onscripter_csharp
+{
+	/// <summary>
+	/// Description of ONScripterLabel.
+	/// </summary>
+	public class ONScripterLabel
+	{
+		public ONScripterLabel()
+		{
+		}
+	}
+}
