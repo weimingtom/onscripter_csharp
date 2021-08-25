ca914c2d8e2fa13c0ad3fc9389031c2b305d55e4
weimingtom <weimingtom@qq.com>
2021-08-26 01:33:16 +0800

    ONScripterLabel.cpp

---
+++ onscripter_csharp/onscripter_csharp/ButtonLink.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: wmt
+ * Date: 2021-8-26
+ * Time: 1:32
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace onscripter_csharp
+{
+	/// <summary>
+	/// Description of ButtonLink.
+	/// </summary>
+	public class ButtonLink
+	{
+		public ButtonLink()
+		{
+		}
+	}
+}
