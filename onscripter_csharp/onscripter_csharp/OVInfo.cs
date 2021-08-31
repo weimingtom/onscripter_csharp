f31577dde4aff14e42b6b4f75a32fb49eeccaf04
weimingtom <weimingtom@qq.com>
2021-08-31 12:48:05 +0800

    ONScripterLabel_sound.cpp

---
+++ onscripter_csharp/onscripter_csharp/OVInfo.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: wmt
+ * Date: 2021-8-31
+ * Time: 12:47
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace onscripter_csharp
+{
+	/// <summary>
+	/// Description of OVInfo.
+	/// </summary>
+	public class OVInfo
+	{
+		public OVInfo()
+		{
+		}
+	}
+}
