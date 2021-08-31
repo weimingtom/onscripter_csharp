becc66c8ecebf0fcdcd8bf1da7c864d8fa2d0772
weimingtom <weimingtom@qq.com>
2021-08-31 13:19:51 +0800

    ONScripterLabel_text.cpp

---
+++ onscripter_csharp/onscripter_csharp/LineBreakType.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: wmt
+ * Date: 2021-8-31
+ * Time: 13:17
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace onscripter_csharp
+{
+	/// <summary>
+	/// Description of LineBreakType.
+	/// </summary>
+	public class LineBreakType
+	{
+		public LineBreakType()
+		{
+		}
+	}
+}
