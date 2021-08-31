d88fb8a2adda76c3a4802692bad4ebb597566af3
weimingtom <weimingtom@qq.com>
2021-08-31 14:34:46 +0800

    ScriptHandler.cpp

---
+++ onscripter_csharp/onscripter_csharp/LogLink.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: wmt
+ * Date: 2021-8-31
+ * Time: 14:31
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace onscripter_csharp
+{
+	/// <summary>
+	/// Description of LogLink.
+	/// </summary>
+	public class LogLink
+	{
+		public LogLink()
+		{
+		}
+	}
+}
---
+++ onscripter_csharp/onscripter_csharp/VariableInfo.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: wmt
+ * Date: 2021-8-31
+ * Time: 14:30
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace onscripter_csharp
+{
+	/// <summary>
+	/// Description of VariableInfo.
+	/// </summary>
+	public class VariableInfo
+	{
+		public VariableInfo()
+		{
+		}
+	}
+}
