bf13942c419c77308095c89dbb49110b2dffdcfd
weimingtom <weimingtom@qq.com>
2021-08-31 10:14:19 +0800

    ONScripterLabel_effect.cpp

---
+++ onscripter_csharp/onscripter_csharp/EffectLink.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: wmt
+ * Date: 2021-8-31
+ * Time: 10:12
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace onscripter_csharp
+{
+	/// <summary>
+	/// Description of EffectLink.
+	/// </summary>
+	public class EffectLink
+	{
+		public EffectLink()
+		{
+		}
+	}
+}
