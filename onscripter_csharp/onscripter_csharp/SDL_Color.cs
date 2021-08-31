ee58118ceb4cc61c3757794229f7048ee579a672
weimingtom <weimingtom@qq.com>
2021-08-31 11:29:40 +0800

    ONScripterLabel_image.cpp

---
+++ onscripter_csharp/onscripter_csharp/SDL_Color.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: wmt
+ * Date: 2021-8-31
+ * Time: 11:28
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace onscripter_csharp
+{
+	/// <summary>
+	/// Description of SDL_Color.
+	/// </summary>
+	public class SDL_Color
+	{
+		public SDL_Color()
+		{
+		}
+	}
+}
