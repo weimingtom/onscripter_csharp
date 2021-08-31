e39f28b86b4f2905d59f7ae5feade1e90126abaf
weimingtom <weimingtom@qq.com>
2021-08-31 10:52:19 +0800

    ONScripterLabel_event.cpp

---
+++ onscripter_csharp/onscripter_csharp/SDL_KeyboardEvent.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: wmt
+ * Date: 2021-8-31
+ * Time: 10:50
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace onscripter_csharp
+{
+	/// <summary>
+	/// Description of SDL_KeyboardEvent.
+	/// </summary>
+	public class SDL_KeyboardEvent
+	{
+		public SDL_KeyboardEvent()
+		{
+		}
+	}
+}
